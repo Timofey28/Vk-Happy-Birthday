19dc58431d410f39237175cec82c55c0586d849d
Timofey Egorov <timegorr@gmail.com>
2024-04-15 15:37:49 +0300

    fixed get_logs.py after ssd failure restoration

--- get_logs.py
+++ get_logs.py
@@ -1,8 +1,12 @@
 import pysftp
 from data import my_Hostname, my_Username, my_Password
+import warnings
+warnings.filterwarnings('ignore')
 
 try:
-    with pysftp.Connection(host=my_Hostname, username=my_Username, password=my_Password) as sftp:
+    cnopts = pysftp.CnOpts()
+    cnopts.hostkeys = None
+    with pysftp.Connection(host=my_Hostname, username=my_Username, password=my_Password, cnopts=cnopts) as sftp:
         print("Connection succesfully established ... ")
         sftp.get('/root/Vk-Happy-Birthday/info.log', 'info.log')
         sftp.get('/root/Vk-Happy-Birthday/congratulations/no.txt', 'no_congratulation.txt')
